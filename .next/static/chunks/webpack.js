e32ff7e7fbcf069ad271e2b11f7293017247d185
rishipv <rpreethamvemula@gmail.com>
2024-08-16 20:09:35 +0530

    added coming soon sub section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f97c325d08b6242"; }
+/******/ 		__webpack_require__.h = function() { return "70d833df5dd456a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
